ae137da83af09f1e38a0899f6e6b1ea836464db3
AmirHosseinAbbasiNezhad <amirabbasinezhad6@gmail.com>
2022-09-15 03:37:59 +0430

    1111

--- src/components/home/Slider.tsx
+++ src/components/home/Slider.tsx
@@ -1,12 +1,6 @@
 import { useEffect, useState } from "react";
 import Slide from "./Slide";
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-import IconButton from '@mui/material/IconButton';
 import banerimage1 from "../../../public/assets/baner1111.png";
-
-import baner22 from "../../../public/assets/banner22.png"
-import baner3 from "../../../public/assets/banner3.png"
 import React from "react";
 const Slider = () => {
     const data = [
